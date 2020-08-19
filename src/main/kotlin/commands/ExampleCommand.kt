6ffc56a6e713aca7cfc819dac6312737204ef490
Dominika <sokolov.dominika@gmail.com>
2020-08-18 21:54:42 -0400

    Return if message isn't a command

--- src/main/kotlin/commands/ExampleCommand.kt
+++ src/main/kotlin/commands/ExampleCommand.kt
@@ -10,19 +10,13 @@ import Command
 object ExampleCommand : Command("ec") {
     init {
         then(literal<Cmd>("1").executes {
-            it.source later {
-                message.channel.send("[$name] First argument!")
-            }
+            it.source later { message.channel.send("[$name] First argument!") }
             0
         }.then(literal<Cmd>("2").executes {
-            it.source later {
-                message.channel.send("[$name] Second argument used after first argument!")
-            }
+            it.source later { message.channel.send("[$name] Second argument used after first argument!") }
             0
         })).then(literal<Cmd>("3").executes {
-            it.source later {
-                message.channel.send("[$name] Second argument used without first argument!")
-            }
+            it.source later { message.channel.send("[$name] Second argument used without first argument!") }
             0
         })
     }
