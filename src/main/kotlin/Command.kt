b3341dca9899ecbd131548af8a2a34cee931a76f
Dominika <sokolov.dominika@gmail.com>
2020-08-18 20:09:30 -0400

    Make Cmd async

--- src/main/kotlin/Command.kt
+++ src/main/kotlin/Command.kt
@@ -1,5 +1,9 @@
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import net.ayataka.kordis.event.events.message.MessageReceiveEvent
+import java.util.concurrent.ConcurrentLinkedQueue
 
 /**
  * @author dominikaaaa
@@ -11,4 +15,18 @@ open class Command(_name: String) : LiteralArgumentBuilder<Cmd>(_name) {
 
 class Cmd(_event: MessageReceiveEvent) {
     val event = _event
+
+    private var asyncQueue: ConcurrentLinkedQueue<suspend MessageReceiveEvent.() -> Unit> = ConcurrentLinkedQueue()
+
+    infix fun later(block: suspend MessageReceiveEvent.() -> Unit) {
+        asyncQueue.add(block)
+    }
+
+    suspend fun file(event: MessageReceiveEvent) {
+        asyncQueue.map {
+            GlobalScope.async {
+                event.it()
+            }
+        }.awaitAll()
+    }
 }
--- src/main/kotlin/commands/ExampleCommand.kt
+++ src/main/kotlin/commands/ExampleCommand.kt
@@ -10,13 +10,19 @@ import Command
 object ExampleCommand : Command("ec") {
     init {
         then(literal<Cmd>("1").executes {
-            println("[$name] First argument!")
+            it.source later {
+                message.channel.send("[$name] First argument!")
+            }
             0
         }.then(literal<Cmd>("2").executes {
-            println("[$name] Second argument used after first argument!")
+            it.source later {
+                message.channel.send("[$name] Second argument used after first argument!")
+            }
             0
         })).then(literal<Cmd>("3").executes {
-            println("[$name] Second argument used without first argument!")
+            it.source later {
+                message.channel.send("[$name] Second argument used without first argument!")
+            }
             0
         })
     }
