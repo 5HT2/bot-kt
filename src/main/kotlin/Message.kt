f444dca2e449ab63bd8bdbad9bd11f86447394bd
Dominika <sokolov.dominika@gmail.com>
2020-08-21 01:07:26 -0400

    Deleted unused classes

--- src/main/kotlin/Message.kt
+++
@@ -1,7 +0,0 @@
-import net.ayataka.kordis.event.events.message.MessageReceiveEvent
-
-object Respond {
-    suspend fun reply(event: MessageReceiveEvent, reply: String) {
-        event.message.channel.send(reply)
-    }
-}
