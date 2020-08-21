071adcabef034470266f2b97d297c2c7689c2238
lv <~@l1v.in>
2020-08-21 14:06:37 -0400

    Formatting in ArchiveCommand

--- src/main/kotlin/commands/ArchiveCommand.kt
+++ src/main/kotlin/commands/ArchiveCommand.kt
@@ -1,6 +1,7 @@
 package commands
 
 import Command
+import Main
 import doesLater
 import net.ayataka.kordis.entity.edit
 import net.ayataka.kordis.entity.server.permission.PermissionSet
@@ -18,35 +19,40 @@ object ArchiveCommand : Command("archive") {
                         color = Main.Colors.WARN.color
                     }
                 }
+                return@doesLater
+            }
+
+            val channel = server?.channels?.find(message.channel.id)
+            val archivedChannelsNum = server?.channels?.filter { c -> c.name.contains(Regex("archived")) }?.size
+
+            val userOverrides: Collection<UserPermissionOverwrite>? =
+                server?.channels?.find(channel!!.id)?.userPermissionOverwrites
+            val roleOverrides: Collection<RolePermissionOverwrite>? =
+                server?.channels?.find(channel!!.id)?.rolePermissionOverwrites
+
+            val allow = PermissionSet(0)
+            val deny = PermissionSet(3072) // read + send
+            val everyoneRole = server?.roles?.filter { r -> r.isEveryone }!!
+            val oldName = channel?.name
+
+            if (userOverrides == null || roleOverrides == null) {
+                message.channel.send("No user/role overrides found!")
             } else {
-                val channel = server?.channels?.find(message.channel.id)
-                val userOverrides: Collection<UserPermissionOverwrite>? =
-                    server?.channels?.find(channel!!.id)?.userPermissionOverwrites
-                val roleOverrides: Collection<RolePermissionOverwrite>? =
-                    server?.channels?.find(channel!!.id)?.rolePermissionOverwrites
-                val allow = PermissionSet(0)
-                val deny = PermissionSet(3072) // read + send
-                val role = server?.roles?.filter { r -> r.isEveryone }!!
-                val archivedChannelsNum = server?.channels?.filter { c -> c.name.contains(Regex("archived")) }?.size
-                val oldName = channel?.name
-                if (userOverrides == null || roleOverrides == null) {
-                    message.channel.send("No user/role overrides found!")
-                } else {
-                    channel?.edit {
-                        this.name = "archived-$archivedChannelsNum"
-                        this.userPermissionOverwrites.removeAll(userOverrides)
-                        this.rolePermissionOverwrites.removeAll(roleOverrides)
-                        this.rolePermissionOverwrites.add(RolePermissionOverwrite(role[0], allow, deny))
-                    }
-                    message.channel.send {
-                        embed {
-                            author(name = message.author?.name)
-                            field("Success", "Changed name from `$oldName` to `archived-$archivedChannelsNum`", true)
-                            color = Main.Colors.SUCCESS.color
-                        }
+                channel?.edit {
+                    this.name = "archived-$archivedChannelsNum"
+                    this.userPermissionOverwrites.removeAll(userOverrides)
+                    this.rolePermissionOverwrites.removeAll(roleOverrides)
+                    this.rolePermissionOverwrites.add(RolePermissionOverwrite(everyoneRole[0], allow, deny))
+                }
+                message.channel.send {
+                    embed {
+                        author(name = message.author?.name)
+                        field("Success", "Changed name from `$oldName` to `archived-$archivedChannelsNum`", true)
+                        color = Main.Colors.SUCCESS.color
                     }
                 }
             }
+
         }
     }
 }
