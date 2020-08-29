6e8eec8f95a8c15b2c7e7d72278019a54a3ba172
wnuke <wnuke@wnuke.dev>
2020-08-29 00:37:23 -0700

    user bulk delete instead of individually deleting each message in purge command

--- src/main/kotlin/commands/PurgeCommand.kt
+++ src/main/kotlin/commands/PurgeCommand.kt
@@ -3,10 +3,8 @@ package commands
 import Command
 import arg
 import doesLater
-import greedyString
 import integer
-import literal
-import string
+import net.ayataka.kordis.entity.deleteAll
 
 object PurgeCommand : Command("purge") {
     init {
@@ -17,9 +15,7 @@ object PurgeCommand : Command("purge") {
                     return@doesLater
                 }
                 val number: Int = context arg "number"
-                for (message in message.channel.getMessages(number)) {
-                    message.delete()
-                }
+                message.channel.getMessages(number).deleteAll()
             }
         }
     }
