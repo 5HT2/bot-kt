b79be6721a960060afec082041f3fb6587471d29
wnuke <wnuke@wnuke.dev>
2020-08-29 01:06:35 -0700

    make per user purge work with mention and tag as well as id

--- src/main/kotlin/commands/PurgeCommand.kt
+++ src/main/kotlin/commands/PurgeCommand.kt
@@ -3,8 +3,8 @@ package commands
 import Command
 import arg
 import doesLater
+import greedyString
 import integer
-import long
 import net.ayataka.kordis.entity.deleteAll
 
 object PurgeCommand : Command("purge") {
@@ -18,16 +18,16 @@ object PurgeCommand : Command("purge") {
                 val number: Int = context arg "number"
                 message.channel.getMessages(number).deleteAll()
             }
-            long("user") {
+            greedyString("user") {
                 doesLater { context ->
                     if (!server!!.members.find(message.author!!.id)!!.canManage(message.serverChannel!!)) {
                         //Main.missingPermissionEmbed(message.channel) assuming my other pr gets merged (#20) this can be used...
                         return@doesLater
                     }
                     val number: Int = context arg "number"
-                    val user: Long = context arg "user"
+                    val user: String = context arg "user"
                     val search = if (number < 1000) number * 2 + 50 else number
-                    message.channel.getMessages(search).filter { it.author!!.id == user }.take(number).deleteAll()
+                    message.channel.getMessages(search).filter { it.author!!.id.toString() == user || it.author!!.mention == user || it.author!!.tag == user }.take(number).deleteAll()
                 }
             }
         }
