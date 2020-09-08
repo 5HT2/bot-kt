0a88a7901c728dc073c9afb6c1ab1736fc7f1f5b
wnuke <wnuke@wnuke.dev>
2020-09-08 15:28:10 -0700

    give error if cant load rules file

--- src/main/kotlin/commands/RulesCommand.kt
+++ src/main/kotlin/commands/RulesCommand.kt
@@ -11,7 +11,8 @@ object RulesCommand : Command("r") {
         string("rule") {
             doesLater { context ->
                 val ruleName: String = context arg "rule"
-                message.channel.send(FileManager.readConfigSafe<RulesConfig>(ConfigType.RULES, false)!!.rules.getOrDefault(ruleName, "No such rule $ruleName."))
+                val rule = FileManager.readConfigSafe<RulesConfig>(ConfigType.RULES, false)?.rules?.getOrDefault(ruleName, "No such rule $ruleName.")?: "Could not load rules file."
+                message.channel.send(rule)
             }
         }
     }
