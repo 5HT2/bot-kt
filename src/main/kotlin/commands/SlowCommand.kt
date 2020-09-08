4b6f36975adbcff15f1bcc7cce4126a88e1d3d7f
wnuke <wnuke@wnuke.dev>
2020-09-08 12:13:47 -0700

    dont autoremove slowmode

--- src/main/kotlin/commands/SlowCommand.kt
+++ src/main/kotlin/commands/SlowCommand.kt
@@ -8,39 +8,27 @@ import StringHelper.MessageTypes.MISSING_PERMISSIONS
 import arg
 import doesLater
 import integer
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 object SlowCommand : Command("slow") {
     init {
         integer("wait") {
-            integer("time") {
-                doesLater { context ->
-                    if (!Permissions.hasPermission(message, PermissionTypes.COUNCIL_MEMBER)) {
-                        StringHelper.sendMessage(message.channel, MISSING_PERMISSIONS)
-                        return@doesLater
-                    }
-                    val wait: Int = context arg "wait"
-                    val time: Int = context arg "time"
-                    val originalWait = message.serverChannel!!.rateLimitPerUser
-                    message.serverChannel!!.edit {
-                        rateLimitPerUser = wait
-                    }
-                    GlobalScope.launch {
-                        delay(time * 1000L)
-                        message.serverChannel!!.edit {
-                            rateLimitPerUser = originalWait
-                        }
-                    }
+            doesLater { context ->
+                if (!Permissions.hasPermission(message, PermissionTypes.COUNCIL_MEMBER)) {
+                    StringHelper.sendMessage(message.channel, MISSING_PERMISSIONS)
+                    return@doesLater
+                }
+                val wait: Int = context arg "wait"
+                message.serverChannel!!.edit {
+                    rateLimitPerUser = wait
                 }
             }
+
         }
     }
 
     override fun getHelpUsage(): String {
-        return "Enables slowmode for this channel, for a specified amount of time\n\n" +
+        return "Enables slowmode for this channel, specifying the wait in seconds.\n\n" +
                 "Usage:\n" +
-                "`;$name 10 60`"
+                "`;$name 10`"
     }
 }
