d31a74157c507995e12a4927d0cbd14fb63bfe34
wnuke <wnuke@wnuke.dev>
2020-09-08 11:03:32 -0700

    update to use new permissions system for reloading rules

--- src/main/kotlin/commands/ReloadRulesCommand.kt
+++ src/main/kotlin/commands/ReloadRulesCommand.kt
@@ -1,6 +1,7 @@
 package commands
 
 import Command
+import StringHelper.MessageTypes.MISSING_PERMISSIONS
 import arg
 import doesLater
 import string
@@ -8,6 +9,10 @@ import string
 object ReloadRulesCommand : Command("reloadrules") {
     init {
         doesLater {
+            if (!Permissions.hasPermission(message, PermissionTypes.COUNCIL_MEMBER)) {
+                StringHelper.sendMessage(message.channel, MISSING_PERMISSIONS)
+                return@doesLater
+            }
             StringHelper.removeServerRules(message.server!!)
             message.channel.send("Rules have been reloaded...")
         }
