c0cd2fb99d2fa4b5eafe6c8af5e8e73f5fc4f0b4
IcyChungus <sourtaste000@gmail.com>
2020-09-09 15:31:19 -0700

    removed jcenter in build.gradle, and send error in text channel when github token is not found/invalid.

--- src/main/kotlin/commands/IssueCommand.kt
+++ src/main/kotlin/commands/IssueCommand.kt
@@ -4,13 +4,17 @@ package commands
 
 import AuthConfig
 import Command
-import classes.issue.Issue
+import ConfigType
+import FileManager
+import Main
 import arg
+import classes.issue.Issue
 import com.google.gson.Gson
 import doesLater
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import string
+import java.io.FileNotFoundException
 
 /**
  * @author sourTaste000
@@ -18,6 +22,7 @@ import string
  */
 object IssueCommand : Command("issue") {
     init {
+        //TODO: Make a tree that defaults to kami-blue for user
         string("user") {
             string("repoName") {
                 string("issueNum") {
@@ -25,17 +30,28 @@ object IssueCommand : Command("issue") {
                         val user: String = context arg "user"
                         val repoName: String = context arg "repoName"
                         val issueNum: String = context arg "issueNum"
-                        val githubToken = FileManager.readConfigSafe<AuthConfig>(ConfigType.AUTH, true)?.githubToken
-                        val request = Request.Builder().addHeader("Authorization", "token $githubToken").url("https://api.github.com/repos/$user/$repoName/issues/$issueNum").get().build()
+                        val githubToken =
+                            FileManager.readConfigSafe<AuthConfig>(ConfigType.AUTH, true)?.githubToken ?: run {
+                                message.channel.send {
+                                    embed {
+                                        title = "[$name]"
+                                        description = "Github Token not found/invalid!"
+                                        color = Main.Colors.ERROR.color
+                                    }
+                                }
+                                return@doesLater
+                            }
+                        val request = Request.Builder().addHeader("Authorization", "token $githubToken")
+                            .url("https://api.github.com/repos/$user/$repoName/issues/$issueNum").get().build()
                         val response = OkHttpClient().newCall(request).execute()
                         println(response.request())
                         val result = Gson().fromJson(response.body()!!.string(), Issue::class.java)
                         message.channel.send(result!!.body)
-                        }
                     }
                 }
             }
         }
+    }
 
     override fun getHelpUsage(): String {
         return "Getting information of an issue on github. \n\n" +
