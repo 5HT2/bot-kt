c64ffdba7339019bf57391ce6707f6a1d6cf28ab
lv <~@l1v.in>
2020-09-10 13:04:31 -0400

    Added setup.sh script for systemd process

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 process="$(psgrep java)"
-currentVersion="$(cat /home/mika/bot-kt/currentVersion)"
+currentVersion="$(cat ~/bot-kt/currentVersion)"
 
 # Run if Java is not running
 if [[ "$process" ]]; then
-  cd /home/mika/bot-kt/
+  cd ~/bot-kt/
   java -jar bot-kt-${currentVersion}.jar
 fi
