02dc9085bed7aea0e0dbcba7b485c7a82b16c500
IcyChungus <47074495+sourTaste000@users.noreply.github.com>
2020-09-13 15:26:23 -0700

    Revert "Initial attempt for issue finding by name" because it breaks everything...
    This reverts commit 004fe0b4b5525e349f2f42be6c95198a613eeba9.

--- src/main/kotlin/classes/search/Search.kt
+++
@@ -1,7 +0,0 @@
-package classes.search
-
-data class Search(
-    var incomplete_results: Boolean = false,
-    var items: List<Item>? = listOf(),
-    var total_count: Int = 0
-)
--- src/main/kotlin/commands/IssueCommand.kt
+++ src/main/kotlin/commands/IssueCommand.kt
@@ -8,10 +8,8 @@ import Main
 import arg
 import classes.issue.Issue
 import classes.pull.Pull
-import classes.search.Search
 import com.google.gson.Gson
 import doesLater
-import greedyString
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import string
@@ -20,7 +18,6 @@ import string
  * @author sourTaste000
  * @since 2020/9/8
  */
-@Suppress("BlockingMethodInNonBlockingContext")
 object IssueCommand : Command("issue") {
     init {
         /*
@@ -28,141 +25,89 @@ object IssueCommand : Command("issue") {
              Make a tree that defaults to kami-blue for user
              Support Multiple Assignees
          */
-        string("mode") {
-            string("user") {
-                string("repoName") {
-                    greedyString("issueNum") {
-                        doesLater { context ->
-                            val mode: String = context arg "mode"
-                            val user: String = context arg "user"
-                            val repoName: String = context arg "repoName"
-                            val issueNum: String = context arg "issueNum"
-                            val githubToken =
-                                FileManager.readConfigSafe<AuthConfig>(ConfigType.AUTH, true)?.githubToken ?: run {
-                                    message.channel.send {
-                                        embed {
-                                            title = "[$name]"
-                                            description = "Github Token not found!"
-                                            color = Main.Colors.ERROR.color
-                                        }
-                                    }
-                                    return@doesLater
-                                }
-                            if (mode == "number") {
-                                val request = Request.Builder().addHeader("Authorization", "token $githubToken").url("https://api.github.com/repos/$user/$repoName/issues/$issueNum").get().build()
-                                val response = OkHttpClient().newCall(request).execute()
-                                println(response.request().toString() + " from user ${message.author?.name}(${message.author?.id})")
-                                val result = Gson().fromJson(response.body()!!.string(), Issue::class.java)
-                                try {
-                                    if (result.html_url.contains("issue")) {
-                                        message.channel.send {
-                                            embed {
-                                                title = result!!.title
-                                                thumbnailUrl = result.user.avatar_url
-                                                color = if (result.state == "closed") { Main.Colors.ERROR.color } else { Main.Colors.SUCCESS.color }
-                                                field(
-                                                    "Description",
-                                                    if (result.body.isEmpty()) {
-                                                        "No description provided."
-                                                    } else {
-                                                        result.body.replace(Regex("<!--.*-->"), "")
-                                                    }, false
-                                                )
-                                                field("Status", result.state, false)
-                                                field(
-                                                    "Milestone",
-                                                    result.milestone?.title ?: "No Milestone Added",
-                                                    false
-                                                )
-                                                field(
-                                                    "Labels",
-                                                    if ((result.labels?.joinToString { it.name } ?: "No Labels").isEmpty()) {
-                                                        "No Labels"
-                                                    } else {
-                                                        result.labels!!.joinToString { it.name }
-                                                    },
-                                                    false
-                                                )
-                                                field(
-                                                    "Assignees",
-                                                    if ((result.assignees?.joinToString { it.login } ?: "No Assignees").isEmpty()) {
-                                                        "No Assignees"
-                                                    } else {
-                                                        result.assignees!!.joinToString { it.login }
-                                                    },
-                                                    false
-                                                )
-                                                author(
-                                                    "カミレッドー！",
-                                                    "https://kamiblue.org",
-                                                    "https://cdn.discordapp.com/avatars/743237292294013013/591c1daf9efcfdd7ea2db1592d818fa6.png"
-                                                )
-                                                url = result.html_url
-                                            }
-                                        }
-                                    } else if (result.html_url.contains("pull")) {
-                                        val requestPull = Request.Builder().addHeader("Authorization", "token $githubToken").url(result.pull_request.url).get().build()
-                                        val responsePull = OkHttpClient().newCall(requestPull).execute()
-                                        println(responsePull.request().toString() + " from user ${message.author?.name}(${message.author?.id})")
-                                        val resultPull = Gson().fromJson(responsePull.body()!!.string(), Pull::class.java)
-                                        message.channel.send {
-                                            embed {
-                                                title = resultPull.title
-                                                thumbnailUrl = resultPull.user.avatar_url
-                                                color = if (resultPull.state == "closed") {
-                                                    Main.Colors.ERROR.color
-                                                } else {
-                                                    Main.Colors.SUCCESS.color
-                                                }
-                                                field(
-                                                    "Description",
-                                                    if (resultPull.body.isEmpty()) {
-                                                        "No description provided."
-                                                    } else {
-                                                        resultPull.body.replace(Regex("<!--.*-->"), "")
-                                                    }, false
-                                                )
-                                                field("Additions", resultPull.additions, false)
-                                                field("Deletions", resultPull.deletions, false)
-                                                field("Commits", resultPull.commits, false)
-                                                field("Changed Files", resultPull.changed_files, false)
-                                                field("Comments", resultPull.comments, false)
-                                                author(
-                                                    "カミレッドー！",
-                                                    "https://kamiblue.org",
-                                                    "https://cdn.discordapp.com/avatars/743237292294013013/591c1daf9efcfdd7ea2db1592d818fa6.png"
-                                                )
-                                            }
-                                        }
-                                    }
-                                } catch (e: Exception) {
-                                    message.channel.send {
-                                        embed {
-                                            title = "Error"
-                                            description = "Something went wrong when trying to execute this command! Does the user/repo/issue exist?"
-                                            field("Stacktrace", "```$e```", false)
-                                            e.printStackTrace()
-                                            color = Main.Colors.ERROR.color
-                                        }
+        string("user") {
+            string("repoName") {
+                string("issueNum") {
+                    doesLater { context ->
+                        val user: String = context arg "user"
+                        val repoName: String = context arg "repoName"
+                        val issueNum: String = context arg "issueNum"
+                        val githubToken =
+                            FileManager.readConfigSafe<AuthConfig>(ConfigType.AUTH, true)?.githubToken ?: run {
+                                message.channel.send {
+                                    embed {
+                                        title = "[$name]"
+                                        description = "Github Token not found!"
+                                        color = Main.Colors.ERROR.color
                                     }
                                 }
-                            } else if (mode == "word") {
-                                try {
-                                    val request = Request.Builder().addHeader("Authorization", "token $githubToken").url("https://api.github.com/search/issues?q=$user/$repoName+${issueNum.replace(" ", "%20")}&sort=created&order=asc").get().build()
-                                    val response = OkHttpClient().newCall(request).execute()
-                                    println(response.request().toString() + " from user ${message.author?.name}(${message.author?.id})")
-                                    val result = Gson().fromJson(response.body()!!.string(), Search::class.java)
-                                    message.channel.send(result.items?.get(0)?.html_url ?: "No results found.")
-                                } catch(e: Exception){
-                                    message.channel.send {
-                                        embed {
-                                            title = "Error"
-                                            description = "Something went wrong when trying to execute this command! Does the user/repo/issue exist?"
-                                            field("Stacktrace", "```$e```", false)
-                                            e.printStackTrace()
-                                            color = Main.Colors.ERROR.color
-                                        }
+                                return@doesLater
+                            }
+                        val request = Request.Builder().addHeader("Authorization", "token $githubToken").url("https://api.github.com/repos/$user/$repoName/issues/$issueNum").get().build()
+                        val response = OkHttpClient().newCall(request).execute()
+                        println(response.request().toString() + " from user ${message.author?.name}(${message.author?.id})")
+                        val result = Gson().fromJson(response.body()!!.string(), Issue::class.java)
+                        try {
+                            if(result.html_url.contains("issue")) {
+                                message.channel.send {
+                                    embed {
+                                        title = result!!.title
+                                        thumbnailUrl = result.user.avatar_url
+                                        color = if (result.state == "closed") { Main.Colors.ERROR.color } else { Main.Colors.SUCCESS.color }
+                                        field(
+                                            "Description",
+                                            if (result.body.isEmpty()) {
+                                                "No description provided."
+                                            } else {
+                                                result.body.replace(Regex("<!--.*-->"), "")
+                                            }, false
+                                        )
+                                        field("Status", result.state, false)
+                                        field("Milestone", result.milestone?.title ?: "No Milestone Added", false)
+                                        field("Labels", if((result.labels?.joinToString { it.name } ?: "No Labels").isEmpty()){"No Labels"} else {result.labels!!.joinToString { it.name }}, false)
+                                        field("Assignees", if((result.assignees?.joinToString { it.login } ?: "No Assignees").isEmpty()){"No Assignees"} else {result.assignees!!.joinToString { it.login }}, false)
+                                        author(
+                                            "カミレッドー！",
+                                            "https://kamiblue.org",
+                                            "https://cdn.discordapp.com/avatars/743237292294013013/591c1daf9efcfdd7ea2db1592d818fa6.png"
+                                        )
+                                        url = result.html_url
                                     }
+                                }
+                            } else if(result.html_url.contains("pull")){
+                                val requestPull = Request.Builder().addHeader("Authorization", "token $githubToken").url(result.pull_request.url).get().build()
+                                val responsePull = OkHttpClient().newCall(requestPull).execute()
+                                println(responsePull.request().toString() + " from user ${message.author?.name}(${message.author?.id})")
+                                val resultPull = Gson().fromJson(responsePull.body()!!.string(), Pull::class.java)
+                                message.channel.send {
+                                    embed{
+                                        title = resultPull.title
+                                        thumbnailUrl = resultPull.user.avatar_url
+                                        color = if (resultPull.state == "closed") { Main.Colors.ERROR.color } else { Main.Colors.SUCCESS.color }
+                                        field(
+                                            "Description",
+                                            if (resultPull.body.isEmpty()) {
+                                                "No description provided."
+                                            } else {
+                                                resultPull.body.replace(Regex("<!--.*-->"), "")
+                                            }, false
+                                        )
+                                        field("Additions", resultPull.additions, false)
+                                        field("Deletions", resultPull.deletions, false)
+                                        field("Commits", resultPull.commits, false)
+                                        field("Changed Files", resultPull.changed_files, false)
+                                        field("Comments", resultPull.comments, false)
+                                    }
+                                }
+                            }
+                        } catch (e: Exception) {
+                            message.channel.send {
+                                embed {
+                                    title = "Error"
+                                    description = "Something went wrong when trying to execute this command! Does the user/repo/issue exist?"
+                                    field("Stacktrace", "```$e```", false)
+                                    e.printStackTrace()
+                                    color = Main.Colors.ERROR.color
                                 }
                             }
                         }
@@ -175,10 +120,8 @@ object IssueCommand : Command("issue") {
     override fun getHelpUsage(): String {
         return "Getting information of an issue/pull on github. \n\n" +
                 "Usage: \n" +
-                "`;$name <user/organization> <repository> <issue | query>`\n\n" +
-                "Example for getting issue by number: \n" +
-                "`;$name kami-blue bot-kt 10`\n" +
-                "Example for getting issue by search: \n" +
-                "`;$name kami-blue client baritone pathfinding`"
+                "`;$name <user/organization> <repository> <issue>`\n\n" +
+                "Example: \n" +
+                "`;$name kami-blue bot-kt 10`\n\n"
     }
 }
