cedcd71ac1d0051a762aa60ad8742e3caede4fa2
Dominika <sokolov.dominika@gmail.com>
2020-09-23 20:01:47 -0400

    Cleanup code

--- src/main/kotlin/commands/ServerInfoCommand.kt
+++ src/main/kotlin/commands/ServerInfoCommand.kt
@@ -1,34 +1,24 @@
 package commands
 
 import Command
-import Main
+import Main.Colors.BLUE
 import doesLater
 
 object ServerInfoCommand : Command("serverinfo") {
     init {
         doesLater {
-            try {
-                message.channel.send {
-                    embed {
-                        color = Main.Colors.BLUE.color
-                        title = server!!.name
-                        thumbnailUrl = server?.icon?.url
-                        server?.owner?.id?.let { it -> field("Owner ID:", it) }
-                        server?.region?.name?.let { it1 -> field("Region:", it1) }
-                        server?.verificationLevel?.name?.let { it2 -> field("Verification Level:", it2, true) }
-                        server?.voiceChannels?.size?.let { it3 -> field("Voice Channels:", it3, true) }
-                        server?.textChannels?.size?.let { it4 -> field("Text Channels:", it4, true) }
-                        server?.members?.size?.let { it5 -> field("Members:", it5, true) }
-                        server?.roles?.size?.let { it7 -> field("Roles:", it7, true) }
-                    }
-                }
-            } catch (e: Exception) {
-                message.channel.send {
-                    embed {
-                        color = Main.Colors.ERROR.color
-                        title = "Something went wrong when executing this command! Here's the stacktrace:"
-                        description = "```$e```"
-                    }
+            message.channel.send {
+                embed {
+                    color = BLUE.color
+                    server?.name?.let { title = it }
+                    server?.icon?.url?.let { thumbnailUrl = it }
+                    server?.owner?.id?.let { field("Owner ID:", it) }
+                    server?.region?.name?.let { field("Region:", it) }
+                    server?.verificationLevel?.name?.let { field("Verification Level:", it, true) }
+                    server?.voiceChannels?.size?.let { field("Voice Channels:", it, true) }
+                    server?.textChannels?.size?.let { field("Text Channels:", it, true) }
+                    server?.members?.size?.let { field("Members:", it, true) }
+                    server?.roles?.size?.let { field("Roles:", it, true) }
                 }
             }
         }
