a193960f0f8539e5cc64eb49e0dbec8a83692f11
sourTaste000 <47074495+sourTaste000@users.noreply.github.com>
2020-09-28 12:12:55 -0700

    reason for ban
    Signed-off-by: sourTaste000 <47074495+sourTaste000@users.noreply.github.com>

--- src/main/kotlin/commands/BanCommand.kt
+++ src/main/kotlin/commands/BanCommand.kt
@@ -5,6 +5,7 @@ import ConfigManager.readConfigSafe
 import UserConfig
 import long
 import integer
+import string
 import doesLater
 import arg
 
@@ -12,17 +13,20 @@ object BanCommand : Command("ec") {
     init {
         long("user") {
             integer("deletemessagedays") {
-                doesLater { context ->
-                    val id: Long = context arg "user"
-                    val messageDays: Int = context arg "deletemessagedays"
-                    val user = Main.client!!.getUser(id)
-                    val server = readConfigSafe<UserConfig>(
-                        ConfigType.USER,
-                        false
-                    )?.primaryServerId?.let { Main.client?.servers?.find(it) } ?: run {
-                        return@doesLater
+                string("reason") {
+                    doesLater { context ->
+                        val id: Long = context arg "user"
+                        val messageDays: Int = context arg "deletemessagedays"
+                        val reason: String = context arg "reason"
+                        val user = Main.client!!.getUser(id)
+                        val server = readConfigSafe<UserConfig>(
+                            ConfigType.USER,
+                            false
+                        )?.primaryServerId?.let { Main.client?.servers?.find(it) } ?: run {
+                            return@doesLater
+                        }
+                        user!!.ban(server, messageDays, reason)
                     }
-                    user!!.ban(server, messageDays)
                 }
             }
         }
