9cd46d619fc07740b464b1cbe6ff400e2cc0ba4e
sourTaste000 <47074495+sourTaste000@users.noreply.github.com>
2020-10-01 08:39:07 -0700

    now sends embed and different role
    Signed-off-by: sourTaste000 <47074495+sourTaste000@users.noreply.github.com>

--- src/main/kotlin/commands/AnnounceCommand.kt
+++ src/main/kotlin/commands/AnnounceCommand.kt
@@ -4,6 +4,7 @@ import Command
 import ConfigManager.readConfigSafe
 import Permissions.hasPermission
 import Send.error
+import Send.normal
 import UserConfig
 import greedyString
 import arg
@@ -13,18 +14,20 @@ object AnnounceCommand : Command("announce") {
     init {
         greedyString("content"){
             doesLater{context ->
-                val content: String? = context arg "content"
-                if (content.isNullOrEmpty()) {
-                    message.error("You need to put announcement here.")
-                    return@doesLater
-                } else if (!message.hasPermission(PermissionTypes.COUNCIL_MEMBER)){
+                val content: String = context arg "content"
+                if (!message.hasPermission(PermissionTypes.ANNOUNCE)){
                     message.error("You don't have the permission to do that!")
                     return@doesLater
                 }
                 message.server?.textChannels?.find(readConfigSafe<UserConfig>(ConfigType.USER, false)?.announceChannel ?: run {
-                    message.error("Bad announcement channel found in config!")
+                    message.error("Please configure the `announcementChannel` in the `${ConfigType.USER.configPath.substring(7)}` config!")
                     return@doesLater
-                })?.send(content)
+                })?.send {
+                    embed {
+                        description = content
+                        color = Colors.primary
+                    }
+                }
             }
         }
     }
