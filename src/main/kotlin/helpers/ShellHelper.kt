6ccd7b36c9bab731af674eebace828629eb25eda
Dominika <sokolov.dominika@gmail.com>
2020-10-02 21:46:15 -0400

    Rewrite ShellHelper

--- src/main/kotlin/helpers/ShellHelper.kt
+++ src/main/kotlin/helpers/ShellHelper.kt
@@ -1,96 +1,50 @@
 package helpers
 
-import Send.stackTrace
-import net.ayataka.kordis.entity.message.Message
+import helpers.ShellHelper.bash
 import java.io.BufferedReader
 import java.io.File
 import java.io.IOException
 import java.io.InputStreamReader
+import java.lang.ProcessBuilder.Redirect.INHERIT
 import java.nio.file.Paths
+
+fun main() {
+    println("ls # /proc/cpuinfo # | grep \"model name\" | cut -c 14-".bash())
+}
 
 object ShellHelper {
     val workingDir = File(Paths.get(System.getProperty("user.dir")).toString())
 
     /**
-     * Run a command and send the stderr to the channel
-     * @return false if command errored
+     * Run a command and send the output to stdout / stderr
+     * @throws IOException
      */
-    suspend fun List<String>.runCommand(message: Message): Boolean {
-        return try {
-            this.runCommand()
-            true
-        } catch (e: IOException) {
-            message.stackTrace(e)
-            false
-        }
-    }
-
-    /**
-     * Run a command and send the stderr to the channel
-     * @return false if command errored
-     */
-    suspend fun String.runCommand(message: Message): Boolean {
-        return try {
-            this.runCommand()
-            true
-        } catch (e: IOException) {
-            message.stackTrace(e)
-            false
-        }
-    }
-
-    /**
-     * Run a command with the output being sent to stdout and stderr
-     */
-    fun List<String>.runCommand() {
-        this.process().systemProcess().start()
-    }
-
-    /**
-     * Run a command with the output being sent to stdout and stderr
-     */
-    fun String.runCommand() {
+    fun String.systemBash() {
         this.process().systemProcess().start()
     }
 
     /**
      * Run a command but return the output
+     * // TODO: stderr does not return
      * @return shell stdout and stderr as a String
+     * @throws IOException
      */
-    fun List<String>.runCommandOutput(): String {
+    fun String.bash(): String {
         return BufferedReader(InputStreamReader(this.process().start().inputStream)).nonNullLines()
     }
 
     /**
-     * Run a command but return the output
-     * @return shell stdout and stderr as a String
-     */
-    fun String.runCommandOutput(): String {
-        return BufferedReader(InputStreamReader(this.process().start().inputStream)).nonNullLines()
-    }
-
-    /**
-     * Example for a piped command:
-     * `ls -l | grep foo` # this turns into # `"/bin/sh", "-c", "\"ls -l| grep foo\""`
-     * @return a process with the working directory set to the path the program is running from
-     */
-    private fun List<String>.process(): ProcessBuilder {
-        return ProcessBuilder(this).directory(workingDir)
-    }
-
-    /**
-     * Splits the single string by spaces into a command
      * @return a process with the working directory set to the path the program is running from
      */
     private fun String.process(): ProcessBuilder {
-        return ProcessBuilder(*split(" ").toTypedArray()).directory(workingDir)
+        return ProcessBuilder(listOf("/bin/bash", "-c", this)).directory(workingDir)
     }
 
     /**
      * @return a process that outputs to stdout and stderr
      */
     private fun ProcessBuilder.systemProcess(): ProcessBuilder {
-        return this.redirectOutput(ProcessBuilder.Redirect.INHERIT).redirectError(ProcessBuilder.Redirect.INHERIT)
+        return this.redirectOutput(INHERIT).redirectError(INHERIT)
     }
 
     /**
