8ee6f5d0400d838230b6286cf6e7ff83fa0f96db
Dominika <sokolov.dominika@gmail.com>
2020-10-02 21:53:52 -0400

    [cleanup] code

--- src/main/kotlin/commands/AnnounceCommand.kt
+++ src/main/kotlin/commands/AnnounceCommand.kt
@@ -1,30 +1,37 @@
 package commands
 
+import Colors
 import Command
 import ConfigManager.readConfigSafe
+import ConfigType
+import PermissionTypes
 import Permissions.hasPermission
 import Send.error
-import Send.normal
 import UserConfig
-import greedyString
 import arg
 import doesLater
+import greedyString
 
 object AnnounceCommand : Command("announce") {
     init {
-        greedyString("content"){
-            doesLater{context ->
+        greedyString("content") {
+            doesLater { context ->
                 val content: String = context arg "content"
-                if (!message.hasPermission(PermissionTypes.ANNOUNCE)){ return@doesLater }
-                message.server?.textChannels?.find(readConfigSafe<UserConfig>(ConfigType.USER, false)?.announceChannel ?: run {
+                if (!message.hasPermission(PermissionTypes.ANNOUNCE)) {
+                    return@doesLater
+                }
+
+                val channel = readConfigSafe<UserConfig>(ConfigType.USER, false)?.announceChannel ?: run {
                     message.error("Please configure the `announcementChannel` in the `${ConfigType.USER.configPath.substring(7)}` config!")
                     return@doesLater
-                })?.send {
+                }
+
+                server?.textChannels?.find(channel)?.send {
                     embed {
                         description = content
                         color = Colors.primary
                     }
-                } ?: run { message.error("Error sending message! Channel couldn't be found") }
+                } ?: run { message.error("Error sending message! Channel `$channel` couldn't be found") }
             }
         }
     }
