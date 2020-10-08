c71e15c4e8bc23e0b94ec8459353f44b0190cbb3
lv <~@l1v.in>
2020-10-08 15:25:28 -0400

    [refactor] Cleanup StringHelper.kt

--- src/main/kotlin/helpers/StringHelper.kt
+++ src/main/kotlin/helpers/StringHelper.kt
@@ -4,29 +4,15 @@ import java.io.File
 import java.net.URL
 
 object StringHelper {
-    fun String.isUrl(): Boolean {
-        return Regex("https?://(www\\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)").matches(
-            this
-        )
-    }
+    fun String.isUrl() = Regex("https?://(www\\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)").matches(this)
 
     fun String.toHumanReadable() = this.toLowerCase().replace("[_-]".toRegex(), " ").capitalize()
 
-    fun String.normalizeCase(): String {
-        return this.substring(0, 1) + this.substring(1).toLowerCase()
-    }
+    fun String.trim(last: Int) = this.substring(0, this.length - last)
 
-    fun String.trim(last: Int): String {
-        return this.substring(0, this.length - last)
-    }
+    fun String.flat(max: Int) = this.substring(0, this.length.coerceAtMost(max))
 
-    fun String.flat(max: Int): String {
-        return this.substring(0, this.length.coerceAtMost(max))
-    }
-
-    fun String.downloadBytes(): ByteArray {
-        return URL(this).readBytes()
-    }
+    fun String.downloadBytes() = URL(this).readBytes()
 
     fun String.writeBytes(url: String): Int {
         val bytes = url.downloadBytes()
