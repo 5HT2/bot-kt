a02714a6748b0afed50da5e7ef609080d2d64dd2
lv <~@l1v.in>
2020-10-16 12:24:13 -0400

    [bump] v1.1.8

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -133,7 +133,7 @@ object Main {
     var process: Job? = null
     var client: DiscordClient? = null
     var ready = false
-    const val currentVersion = "1.1.7"
+    const val currentVersion = "v1.1.8"
 
     private var defaultPrefix: Char? = null
 
