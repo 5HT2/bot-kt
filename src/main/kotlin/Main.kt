19ff1a6144bb2171c22d0c8045821f37b73240b2
Dominika <sokolov.dominika@gmail.com>
2020-10-29 17:52:08 -0400

    [bump] v1.2.4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -139,7 +139,7 @@ object Main {
     var process: Job? = null
     var client: DiscordClient? = null
     var ready = false
-    const val currentVersion = "v1.2.3"
+    const val currentVersion = "v1.2.4"
 
     private var defaultPrefix: Char? = null
 
