dffb1b05952574762fa0e0de82d62ff6a8eb014b
lv <~@l1v.in>
2020-10-31 16:32:49 -0400

    [feature] Message.removeReaction and Message.getReaction

--- src/main/kotlin/helpers/StringHelper.kt
+++ src/main/kotlin/helpers/StringHelper.kt
@@ -2,6 +2,7 @@ package helpers
 
 import java.io.File
 import java.net.URL
+import java.net.URLEncoder
 
 object StringHelper {
     fun String.isUrl() = Regex("https?://(www\\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)").matches(this)
@@ -15,6 +16,8 @@ object StringHelper {
     fun String.flat(max: Int) = this.substring(0, this.length.coerceAtMost(max))
 
     fun String.readBytes() = URL(this).readBytes()
+
+    fun String.uriEncode(): String = URLEncoder.encode(this, "utf-8")
 
     fun String.writeBytes(url: String): Int {
         val bytes = url.readBytes()
