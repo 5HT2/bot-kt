3a92378b335aae7f6ae167f93605a4734faf150f
Dominika <sokolov.dominika@gmail.com>
2020-11-13 14:54:24 -0500

    [test] UserStringReader

--- src/main/kotlin/helpers/DiscordUserArgumentType.kt
+++ src/main/kotlin/helpers/DiscordUserArgumentType.kt
@@ -11,16 +11,39 @@ import net.ayataka.kordis.entity.user.User
 typealias UserPromise = (suspend () -> User?)
 
 val NO_READER_EXCEPTION = SimpleCommandExceptionType(LiteralMessage("There was no reader to read the argument."))
-val NOT_IN_PING_FORMAT_EXCEPTION = DynamicCommandExceptionType { LiteralMessage("The string '$it' is not in ping format.") }
+val NOT_IN_PING_FORMAT_EXCEPTION = DynamicCommandExceptionType { LiteralMessage("The string `$it` is not in ping format.") }
 
 object DiscordUserArgumentType : ArgumentType<UserPromise> {
     override fun parse(reader: StringReader?): UserPromise {
-        val reader = reader ?: throw NO_READER_EXCEPTION.create()
-        val str = reader.readString()
-        val idStr = str.removePrefix("<@!").removePrefix("<@").removeSuffix(">")
-        val id = idStr.toLongOrNull() ?: throw NOT_IN_PING_FORMAT_EXCEPTION.create(str)
+        reader ?: throw NO_READER_EXCEPTION.create()
+        val stringReader = UserStringReader(reader)
+        val str = stringReader.readString()
+//        val idStr = str.replace(Regex("^[<@!]{0,3}"), "").replace(Regex(">$"), "")
+//        val idStr = str.removePrefix("<@!").removePrefix("<@").removeSuffix(">")
+        val id = str.toLongOrNull() ?: throw NOT_IN_PING_FORMAT_EXCEPTION.create(str)
         return {
             Main.client?.getUser(id)
         }
     }
+}
+
+class UserStringReader(stringReader: StringReader?) : StringReader(stringReader) {
+    override fun readString(): String {
+        if (!canRead()) {
+            return ""
+        }
+
+        return readUnquotedString()
+    }
+
+    override fun readUnquotedString(): String {
+        val start = cursor
+        while (canRead() && isAllowedInString(peek())) {
+            skip()
+        }
+        return string.substring(start, cursor)
+    }
+
+    private fun isAllowedInString(c: Char) = c in '0'..'9' || c in 'A'..'Z' || c in 'a'..'z'
+            || c == '_' || c == '-' || c == '.' || c == '+' || c == '<' || c == '>' || c == '@' || c == '!' || c == '#' || c == '&'
 }
