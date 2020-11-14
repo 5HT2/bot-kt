012d86b4d6120b65f2e726968502156db161a806
Dominika <sokolov.dominika@gmail.com>
2020-11-14 11:16:19 -0500

    [fix] Throw exception when in UUIDManager

--- src/main/kotlin/UUIDManager.kt
+++ src/main/kotlin/UUIDManager.kt
@@ -4,5 +4,9 @@ import org.kamiblue.capeapi.AbstractUUIDManager
 import java.io.File
 
 object UUIDManager : AbstractUUIDManager("cache/uuids.json") {
-    override fun log(message: String) { }
+    override fun log(message: String) {
+        throw UUIDFormatException(message)
+    }
+
+    class UUIDFormatException(s: String) : IllegalArgumentException(s)
 }
