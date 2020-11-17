ac4b03e9754d755d8fa83bb5df1d93ff41b3b0a9
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-11-16 20:30:24 -0500

    Cleaned up sourtaste code

--- src/main/kotlin/commands/CounterCommand.kt
+++ src/main/kotlin/commands/CounterCommand.kt
@@ -8,7 +8,6 @@ import org.kamiblue.botkt.utils.MessageSendUtils.error
 import org.kamiblue.botkt.utils.MessageSendUtils.success
 import org.kamiblue.botkt.utils.authenticatedRequest
 import org.kamiblue.botkt.utils.request
-import org.l1ving.api.download.Asset
 import org.l1ving.api.download.Download
 
 object CounterCommand : Command("counter") {
@@ -18,12 +17,16 @@ object CounterCommand : Command("counter") {
             val userPath = ConfigType.USER.configPath.substring(7)
             val config = readConfigSafe<CounterConfig>(ConfigType.COUNTER, false)
 
-            if (config?.downloadEnabled != true && config?.memberEnabled != true) {
-                message.error("Counters are not configured / enabled!")
-            } else if (config.downloadEnabled != true) {
-                message.error("Download counter is not enabled in the `$path` config!")
-            } else if (config.memberEnabled != true) {
-                message.error("Member counter is not enabled in the `$path` config!")
+            when {
+                config?.downloadEnabled != true && config?.memberEnabled != true -> {
+                    message.error("Counters are not configured / enabled!")
+                }
+                config.downloadEnabled != true -> {
+                    message.error("Download counter is not enabled in the `$path` config!")
+                }
+                config.memberEnabled != true -> {
+                    message.error("Member counter is not enabled in the `$path` config!")
+                }
             }
 
             if (updateChannel()) {
@@ -43,69 +46,39 @@ object CounterCommand : Command("counter") {
 
         val server = readConfigSafe<UserConfig>(ConfigType.USER, false)?.primaryServerId?.let {
             Main.client.servers.find(it)
-        } ?: run {
-            return false
-        }
+        } ?: return false
 
-        var downloadStable: Download? = null
-        var downloadNightly: Download? = null
-
-        var updated = false
         val perPage = config.perPage ?: 200
 
-        GitHubUtils.getGithubToken(null)?.let {
-            downloadStable = config.downloadStableUrl?.let { it1 -> authenticatedRequest<Download>("token", it, formatApiUrl(it1, perPage)) }
-            downloadNightly = config.downloadNightlyUrl?.let { it1 -> authenticatedRequest<Download>("token", it, formatApiUrl(it1, perPage)) }
+        val download = GitHubUtils.getGithubToken(null)?.let { token ->
+            config.downloadNightlyUrl?.let { authenticatedRequest<Download>("token", token, formatApiUrl(it, perPage)) } // Nightly
+                    ?: config.downloadStableUrl?.let { authenticatedRequest<Download>("token", token, formatApiUrl(it, perPage)) } // Stable
         } ?: run {
-            downloadStable = config.downloadStableUrl?.let { request<Download>(formatApiUrl(it, perPage)) }
-            downloadNightly = config.downloadNightlyUrl?.let { request<Download>(formatApiUrl(it, perPage)) }
+            config.downloadNightlyUrl?.let { request<Download>(formatApiUrl(it, perPage)) }
+                    ?: config.downloadStableUrl?.let { request<Download>(formatApiUrl(it, perPage)) }
         }
 
-        var totalCount = 0
-        var latestCount = 0
         val memberCount = server.members.size
+        val downLoadCount = download?.countDownload()
 
-        downloadStable?.let {
-            totalCount += countedDownloads(it)
-            latestCount = it[0].assets.count()
+        return if (downLoadCount != null && memberCount != 0) {
+            edit(config, server, downLoadCount.first, downLoadCount.second, memberCount)
+            true
+        } else {
+            false
         }
-
-        downloadNightly?.let {
-            totalCount += countedDownloads(it)
-            latestCount = it[0].assets.count() // nightly will be newer, so we assign it again if nightly isn't null
-        }
-
-        if (totalCount != 0 || latestCount != 0 || memberCount != 0) {
-            edit(config, server, totalCount, latestCount, memberCount)
-            updated = true
-        }
-
-        return updated
-    }
-
-    private suspend fun edit(config: CounterConfig, server: Server, totalCount: Int, latestCount: Int, memberCount: Int) {
-        val totalChannel = config.downloadChannelTotal?.let { server.voiceChannels.find(it) }
-        val latestChannel = config.downloadChannelLatest?.let { server.voiceChannels.find(it) }
-        val memberChannel = config.memberChannel?.let { server.voiceChannels.find(it) }
-
-        totalChannel?.let { it.edit { name = "$totalCount Downloads" } }
-        latestChannel?.let { it.edit { name = "$latestCount Nightly DLs" } }
-        memberChannel?.let { it.edit { name = "$memberCount Members" } }
     }
 
     private fun formatApiUrl(repo: String, perPage: Int) = "https://api.github.com/repos/$repo/releases?per_page=$perPage"
 
-    private fun countedDownloads(download: Download): Int {
-        var total = 0
-        download.forEach { release ->
-            total += release.assets.count()
-        }
-        return total
+    private fun Download.countDownload(): Pair<Int, Int>? {
+        return this.sumBy { release -> release.assets.sumBy { it.download_count } } to
+                this[0].assets.sumBy { it.download_count }
     }
 
-    private fun List<Asset>.count(): Int {
-        var total = 0
-        this.forEach { total += it.download_count }
-        return total
+    private suspend fun edit(config: CounterConfig, server: Server, totalCount: Int, latestCount: Int, memberCount: Int) {
+        config.downloadChannelTotal?.let { server.voiceChannels.find(it)?.edit { name = "$totalCount Downloads" } }
+        config.downloadChannelLatest?.let { server.voiceChannels.find(it)?.edit { name = "$latestCount Nightly DLs" } }
+        config.memberChannel?.let { server.voiceChannels.find(it)?.edit { name = "$memberCount Members" } }
     }
 }
