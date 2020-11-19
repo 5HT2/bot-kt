c0213e17784a097d2e7a03d14172419920b1ce0f
Dominika <sokolov.dominika@gmail.com>
2020-11-19 11:34:57 -0500

    [bump] v1.2.9

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -170,7 +170,7 @@ object Main {
     var capeCommitProcess: Job? = null
     var client: DiscordClient? = null
     var ready = false
-    const val currentVersion = "v1.2.8"
+    const val currentVersion = "v1.2.9"
 
     private var defaultPrefix: Char? = null
 
