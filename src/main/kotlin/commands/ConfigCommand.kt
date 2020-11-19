0d76bff0fe4dd70bc51595ae77dd8c2b26dd307e
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-11-19 12:23:25 -0500

    [cleanup] Cleaned up ConfigCommand

--- src/main/kotlin/commands/ConfigCommand.kt
+++ src/main/kotlin/commands/ConfigCommand.kt
@@ -1,28 +1,28 @@
 package org.kamiblue.botkt.commands
 
 import com.google.gson.GsonBuilder
-import org.kamiblue.botkt.utils.StringUtils.writeBytes
 import org.kamiblue.botkt.*
+import org.kamiblue.botkt.utils.Colors
 import org.kamiblue.botkt.utils.MessageSendUtils.error
 import org.kamiblue.botkt.utils.MessageSendUtils.success
-import org.kamiblue.botkt.utils.Colors
+import org.kamiblue.botkt.utils.StringUtils.writeBytes
+import kotlin.math.min
 
 object ConfigCommand : Command("config") {
+    private val gson = GsonBuilder().setPrettyPrinting().create()
+
     init {
         literal("print") {
             string("name") {
                 doesLaterIfHas(PermissionTypes.MANAGE_CONFIG) { context ->
                     val name: String = context arg "name"
-                    val gson = GsonBuilder().setPrettyPrinting().create()
 
-                    ConfigType.values().forEach { config ->
-                        if (config.configPath.substring(7).toLowerCase() == name.toLowerCase()) {
-                            ConfigManager.readConfig<Any>(config, false)?.let {
-                                message.channel.send("```json\n" + gson.toJson(it) + "\n```")
-                            } ?: message.error("Couldn't find config file, or config is in invalid format")
-
-                            return@doesLaterIfHas
-                        }
+                    ConfigType.values().find {
+                        it.configPath.substring(7).toLowerCase() == name.toLowerCase()
+                    }?.let { config ->
+                        ConfigManager.readConfig<Any>(config, false)?.let {
+                            message.channel.send("```json\n" + gson.toJson(it) + "\n```")
+                        } ?: message.error("Couldn't find config file, or config is in invalid format")
                     }
                 }
             }
@@ -33,8 +33,7 @@ object ConfigCommand : Command("config") {
                 message.channel.send {
                     embed {
                         title = "Config Types"
-                        description = ConfigType.values()
-                            .joinToString { "`${it.configPath.substring(7)}`" } // trim config/ from name
+                        description = ConfigType.values().joinToString { "`${it.configPath.substring(7)}`" } // trim config/ from name
                         color = Colors.PRIMARY.color
                     }
                 }
@@ -73,20 +72,19 @@ object ConfigCommand : Command("config") {
                     doesLaterIfHas(PermissionTypes.MANAGE_CONFIG) { context ->
                         val name: String = context arg "name"
                         val url: String = context arg "url"
-                        val size: Int
 
                         message.success("Downloading `$name`...")
 
                         try {
-                            size = "config/$name".writeBytes(url)
+                            val size = "config/$name".writeBytes(url)
+                            message.success("Successfully downloaded `$name` (${size / 1000.0}KB)!")
                         } catch (e: Exception) {
-                            message.error("Failed to download `$name`\n```${e.stackTrace.joinToString { it.toString() }}```".substring(
-                                0,
-                                512) + "```")
-                            return@doesLaterIfHas
+                            val stackTrace = e.stackTrace.toList().run {
+                                subList(0, min(10, this.size))
+                            }
+                            message.error("Failed to download `$name`\n" +
+                                    "```${stackTrace}```")
                         }
-
-                        message.success("Successfully downloaded `$name` (${size / 1000.0}KB)!")
                     }
                 }
             }
