2c4662278eae98bf2d19d88d79b4b99e4cc2b22b
lv <~@l1v.in>
2020-12-02 10:00:59 -0500

    [feature] ;release command

---
+++ src/main/kotlin/commands/ReleaseCommand.kt
@@ -0,0 +1,24 @@
+package org.kamiblue.botkt.commands
+
+import org.kamiblue.botkt.Command
+import org.kamiblue.botkt.PermissionTypes.CREATE_RELEASE
+import org.kamiblue.botkt.doesLaterIfHas
+import org.kamiblue.botkt.helpers.ShellHelper.systemBash
+import org.kamiblue.botkt.literal
+
+// TODO hardcoded, pending plugin support
+object ReleaseCommand : Command("release") {
+    init {
+        literal("major") {
+            doesLaterIfHas(CREATE_RELEASE) {
+                "./scripts/runAutomatedRelease.sh major".systemBash("/home/mika/kamiblue")
+            }
+        }
+
+        literal("beta") {
+            doesLaterIfHas(CREATE_RELEASE) {
+                "./scripts/runAutomatedRelease.sh".systemBash("/home/mika/kamiblue")
+            }
+        }
+    }
+}
