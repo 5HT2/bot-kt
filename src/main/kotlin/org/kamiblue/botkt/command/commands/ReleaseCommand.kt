bf774364895957840769720916673ba55361a14f
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-12-04 18:04:04 -0500

    Covered LmgtfyCommand, PingCommand, PurgeCommand, ReleaseCommand to new command base

--- src/main/kotlin/org/kamiblue/botkt/command/commands/ReleaseCommand.kt
+++ src/main/kotlin/org/kamiblue/botkt/command/commands/ReleaseCommand.kt
@@ -1,28 +1,24 @@
 package org.kamiblue.botkt.command.commands
 
-import org.kamiblue.botkt.PermissionTypes.CREATE_RELEASE
-import org.kamiblue.botkt.command.CommandOld
-import org.kamiblue.botkt.command.doesLaterIfHas
-import org.kamiblue.botkt.command.literal
+import org.kamiblue.botkt.PermissionTypes
+import org.kamiblue.botkt.command.BotCommand
 import org.kamiblue.botkt.helpers.ShellHelper.systemBash
 
 // TODO hardcoded, pending plugin support
-object ReleaseCommand : CommandOld("release") {
+object ReleaseCommand : BotCommand(
+    name = "release"
+) {
     init {
         literal("major") {
-            doesLaterIfHas(CREATE_RELEASE) {
+            executeIfHas(PermissionTypes.CREATE_RELEASE) {
                 "./scripts/runAutomatedRelease.sh major".systemBash("/home/mika/kamiblue")
             }
         }
 
         literal("beta") {
-            doesLaterIfHas(CREATE_RELEASE) {
+            executeIfHas(PermissionTypes.CREATE_RELEASE) {
                 "./scripts/runAutomatedRelease.sh".systemBash("/home/mika/kamiblue")
             }
         }
-    }
-
-    override fun getHelpUsage(): String {
-        return "Usage:\n`$fullName major`\n`$fullName beta`"
     }
 }
