7f35943765f17ed0c9e82b3706839b031479383b
Dominika <sokolov.dominika@gmail.com>
2020-12-05 12:00:41 -0500

    [fix] Say command sending not working on announcement channels

--- src/main/kotlin/org/kamiblue/botkt/command/MessageExecuteEvent.kt
+++ src/main/kotlin/org/kamiblue/botkt/command/MessageExecuteEvent.kt
@@ -1,12 +1,13 @@
 package org.kamiblue.botkt.command
 
 import net.ayataka.kordis.entity.server.Server
+import net.ayataka.kordis.entity.server.channel.ServerChannel
 import net.ayataka.kordis.event.events.message.MessageEvent
 import net.ayataka.kordis.event.events.message.MessageReceiveEvent
 import org.kamiblue.command.ArgIdentifier
 import org.kamiblue.command.ExecuteEvent
 
-class MessageExecuteEvent (
+class MessageExecuteEvent(
     args: Array<String>,
     event: MessageReceiveEvent
 ) : ExecuteEvent(CommandManager, args), MessageEvent {
@@ -14,6 +15,6 @@ class MessageExecuteEvent (
     override val server: Server? = event.server
     val message = event.message
 
-    fun ArgIdentifier<Long>.getChannelOrNull() = server?.channels?.find(this.value)
-
+    fun ArgIdentifier<Long>.getSendableChannelOrNull(): ServerChannel? =
+        server?.textChannels?.find(this.value) ?: server?.announcementChannels?.find(this.value) ?: server?.storeChannels?.find(this.value)
 }
