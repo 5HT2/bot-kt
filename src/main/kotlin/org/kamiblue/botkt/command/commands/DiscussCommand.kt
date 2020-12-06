340d5138e414b4df57edbb68d7c91cede0aef25d
Dominika <sokolov.dominika@gmail.com>
2020-12-06 15:14:27 -0500

    [refactor] DiscussCommand to new command system

--- src/main/kotlin/org/kamiblue/botkt/command/commands/DiscussCommand.kt
+++ src/main/kotlin/org/kamiblue/botkt/command/commands/DiscussCommand.kt
@@ -3,18 +3,20 @@ package org.kamiblue.botkt.command.commands
 import net.ayataka.kordis.entity.server.permission.PermissionSet
 import net.ayataka.kordis.entity.server.permission.overwrite.RolePermissionOverwrite
 import org.kamiblue.botkt.PermissionTypes
-import org.kamiblue.botkt.command.*
+import org.kamiblue.botkt.command.BotCommand
 import org.kamiblue.botkt.utils.Colors
 
 // TODO: make this not hardcoded
-object DiscussCommand : CommandOld("discuss") {
+object DiscussCommand : BotCommand(
+    name = "discuss",
+    description = "Create and participate in Discussions"
+) {
     init {
         literal("addon") {
-            greedyString("idea") {
-                doesLaterIfHas(PermissionTypes.COUNCIL_MEMBER) { context ->
-                    val idea: String = context arg "idea"
+            greedy("idea") { idea ->
+                executeIfHas(PermissionTypes.COUNCIL_MEMBER) {
                     val name = "t-" + server!!.channels.find(message.channel.id)!!.name.substring(2)
-                    val discussionTopic = server!!.textChannels.findByName(name)!!
+                    val discussionTopic = server.textChannels.findByName(name)!!
 
                     discussionTopic.send {
                         if (!message.attachments.isEmpty()) {
@@ -25,7 +27,7 @@ object DiscussCommand : CommandOld("discuss") {
                         } else { // needs to be else because you cannot embed images in the same message with content
                             embed {
                                 author(name = message.author!!.name)
-                                field("Added:", idea, false)
+                                field("Added:", idea.value, false)
                                 color = Colors.PRIMARY.color
                             }
                         }
@@ -35,20 +37,19 @@ object DiscussCommand : CommandOld("discuss") {
             }
         }
 
-        string("topic") {
-            greedyString("description") {
-                doesLaterIfHas(PermissionTypes.COUNCIL_MEMBER) { context ->
+        string("topic") { topicArg ->
+            greedy("description") { description ->
+                executeIfHas(PermissionTypes.COUNCIL_MEMBER) {
                     val upperCouncil = server!!.roles.findByName("upper council")!!
-                    val lowerCouncil = server!!.roles.findByName("lower council")!!
+                    val lowerCouncil = server.roles.findByName("lower council")!!
 
-                    val topic: String = context arg "topic"
-                    val description: String = context arg "description"
-                    val discussionCategory = server!!.channelCategories.findByName("discussions")
+                    val discussionCategory = server.channelCategories.findByName("discussions")
 
                     val allow = PermissionSet(1024) // allow reading
                     val deny = PermissionSet(2048) // disallow speaking
 
-                    val discussionTopic = server!!.createTextChannel {
+                    val topic = topicArg.value
+                    val discussionTopic = server.createTextChannel {
                         this.category = discussionCategory
                         this.name = "t-$topic"
                     }
@@ -56,7 +57,7 @@ object DiscussCommand : CommandOld("discuss") {
                     discussionTopic.send {
                         embed {
                             author(name = "${message.author!!.name} created $topic")
-                            field("Topic", description, false)
+                            field("Topic", description.value, false)
                             color = Colors.PRIMARY.color
                         }
                     }
@@ -66,20 +67,12 @@ object DiscussCommand : CommandOld("discuss") {
                         this.rolePermissionOverwrites.add(RolePermissionOverwrite(lowerCouncil, allow, deny))
                     }
 
-                    server!!.createTextChannel {
+                    server.createTextChannel {
                         this.category = discussionCategory
                         this.name = "d-$topic"
                     }
                 }
             }
         }
-    }
-
-    override fun getHelpUsage(): String {
-        return "Discuss any topic asynchronously with the council.\n\n" +
-            "Create a new discussion:\n" +
-            "`$fullName <topic-name> <Some detailed description of the topic>`\n\n" +
-            "Add to the current discussion, while inside a `#d-` channel:\n" +
-            "`$fullName addon <Some point to add on. Put a . here if you're attaching an image>`"
     }
 }
