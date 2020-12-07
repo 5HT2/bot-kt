b69bb499ecbd0b1371016731a9dad44960b5befc
Dominika <sokolov.dominika@gmail.com>
2020-12-06 19:24:34 -0500

    [cleanup] Remove CommandOld and Brigadier entirely

--- src/main/kotlin/org/kamiblue/botkt/utils/Exceptions.kt
+++
@@ -1,6 +0,0 @@
-package org.kamiblue.botkt.utils
-
-import com.mojang.brigadier.LiteralMessage
-import com.mojang.brigadier.exceptions.SimpleCommandExceptionType
-
-val NO_READER_EXCEPTION = SimpleCommandExceptionType(LiteralMessage("There was no reader to read the argument."))
