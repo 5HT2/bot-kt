f6a7818217096c639553d69e2459e9f5a7857f40
Dominika <sokolov.dominika@gmail.com>
2020-12-07 20:40:38 -0500

    [bump] v1.4.1

--- src/main/kotlin/org/kamiblue/botkt/Main.kt
+++ src/main/kotlin/org/kamiblue/botkt/Main.kt
@@ -24,7 +24,7 @@ object Main {
             }
         }
 
-    const val currentVersion = "v1.4.0"
+    const val currentVersion = "v1.4.1"
 
     @JvmStatic
     fun main(vararg args: String) = runBlocking {
