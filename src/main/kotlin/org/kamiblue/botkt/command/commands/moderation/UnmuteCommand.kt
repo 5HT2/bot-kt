e05fbe5dca5d6dabc41d0259ab24c92db62865f9
Dominika <sokolov.dominika@gmail.com>
2020-12-11 23:28:23 -0500

    [fix] Formatting

--- src/main/kotlin/org/kamiblue/botkt/command/commands/moderation/UnmuteCommand.kt
+++ src/main/kotlin/org/kamiblue/botkt/command/commands/moderation/UnmuteCommand.kt
@@ -1,10 +1,12 @@
 package org.kamiblue.botkt.command.commands.moderation
 
 import net.ayataka.kordis.entity.find
+import org.kamiblue.botkt.Main
 import org.kamiblue.botkt.MuteManager
 import org.kamiblue.botkt.PermissionTypes
 import org.kamiblue.botkt.command.BotCommand
 import org.kamiblue.botkt.command.Category
+import org.kamiblue.botkt.command.commands.system.ExceptionCommand
 import org.kamiblue.botkt.utils.Colors
 import org.kamiblue.botkt.utils.MessageSendUtils.error
 import org.kamiblue.botkt.utils.MessageSendUtils.success
@@ -30,23 +32,54 @@ object UnmuteCommand : BotCommand(
 
                 val serverMuteInfo = MuteManager.serverMap.getOrPut(server.id) { MuteManager.ServerMuteInfo(server) }
 
-
                 if (serverMuteInfo.muteMap.remove(member.id) != null) {
                     try {
-                        serverMuteInfo.coroutineMap.remove(member.id)?.cancel()
-                        member.removeRole(serverMuteInfo.getMutedRole())
-                        message.success("${member.name}#${member.discriminator} is unmuted")
+                        member.getPrivateChannel().send {
+                            embed {
+                                field(
+                                    "You were unmuted by:",
+                                    "${message.author?.mention ?: "Mute message author not found!"}, in the guild `${server.name}`"
+                                )
+                                color = Colors.SUCCESS.color
+                                footer("ID: ${message.author?.id}", message.author?.avatar?.url)
+                            }
+                        }
                     } catch (e: Exception) {
                         message.channel.send {
                             embed {
+                                title = "Error"
+                                description = "I couldn't DM that user the unmute, they might have had DMs disabled."
+                                color = Colors.ERROR.color
+                            }
+                        }
+                    }
+
+                    try {
+                        serverMuteInfo.coroutineMap.remove(member.id)?.cancel()
+                        member.removeRole(serverMuteInfo.getMutedRole())
+                        message.channel.send {
+                            embed {
+                                field(
+                                    "${member.name}#${member.discriminator} was unmute by:",
+                                    message.author?.mention ?: "Mute message author not found!"
+                                )
+                                footer("ID: ${member.id}", member.avatar.url)
+                                color = Colors.SUCCESS.color
+                            }
+                        }
+                    } catch (e: Exception) {
+                        ExceptionCommand.addException(e)
+                        message.channel.send {
+                            embed {
                                 title = "Unable to unmute ${member.mention}"
-                                description = e.message
+                                description = "${e.message}\n" +
+                                    "Use the `${Main.prefix}exception` command to view the full stacktrace."
                                 color = Colors.ERROR.color
                             }
                         }
                     }
                 } else {
-                    message.error("${member.name}#${member.discriminator} is not muted")
+                    message.error("${member.mention} is not muted")
                 }
             }
         }
