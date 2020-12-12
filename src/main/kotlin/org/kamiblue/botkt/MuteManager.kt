536d6b3a9c65f1d94429fc576f3dc51ab28d46fa
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-12-12 18:42:30 -0500

    [feature] Added mute command (#349)
    Co-authored-by: Dominika <sokolov.dominika@gmail.com>

---
+++ src/main/kotlin/org/kamiblue/botkt/MuteManager.kt
@@ -0,0 +1,166 @@
+package org.kamiblue.botkt
+
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.*
+import net.ayataka.kordis.entity.botUser
+import net.ayataka.kordis.entity.everyone
+import net.ayataka.kordis.entity.server.Server
+import net.ayataka.kordis.entity.server.member.Member
+import net.ayataka.kordis.entity.server.permission.PermissionSet
+import net.ayataka.kordis.entity.server.role.Role
+import net.ayataka.kordis.event.EventHandler
+import net.ayataka.kordis.event.events.server.user.UserJoinEvent
+import net.ayataka.kordis.event.events.server.user.UserRoleUpdateEvent
+import net.ayataka.kordis.utils.timer
+import org.kamiblue.botkt.utils.Colors
+import java.io.*
+import java.util.concurrent.ConcurrentHashMap
+
+object MuteManager {
+
+    val serverMap = HashMap<Long, ServerMuteInfo>() // <Server ID, ServerMuteInfo>
+    private val gson = GsonBuilder().setPrettyPrinting().create()
+    private val type = object : TypeToken<LinkedHashMap<Long, Map<Long, Long>>>() {}.type
+    private val muteFile = File("config/mute.json")
+    private val muteManagerScope = CoroutineScope(Dispatchers.Default)
+
+    fun save() {
+        BufferedWriter(FileWriter(muteFile)).use {
+            val cacheMap = LinkedHashMap<Long, Map<Long, Long>>()
+
+            for ((id, serverMuteInfo) in serverMap) {
+                cacheMap[id] = serverMuteInfo.muteMap
+            }
+
+            gson.toJson(cacheMap, it)
+        }
+    }
+
+    fun load() {
+        BufferedReader(FileReader(muteFile)).use { reader ->
+            val cacheMap = gson.fromJson<LinkedHashMap<Long, Map<Long, Long>>>(reader, type)
+            for ((id, cacheMuteMap) in cacheMap) {
+                serverMap.getOrPut(id) {
+                    ServerMuteInfo(Main.client.servers.find(id)!!)
+                }.apply {
+                    muteMap.clear()
+                    muteMap.putAll(cacheMuteMap)
+                }
+            }
+        }
+    }
+
+    @Suppress("UNUSED")
+    @EventHandler
+    suspend fun userJoinedListener(event: UserJoinEvent) {
+        reAdd(event.member)
+    }
+
+    @Suppress("UNUSED")
+    @EventHandler
+    suspend fun onUserUpdateRoles(event: UserRoleUpdateEvent) {
+        val mutedRole = serverMap[event.server.id]?.getMutedRole() ?: return
+        if (event.before.contains(mutedRole) && !event.member.roles.contains(mutedRole)) {
+            reAdd(event.member)
+        }
+    }
+
+    private suspend fun reAdd(member: Member) {
+        serverMap[member.server.id]?.let { serverMuteInfo ->
+            serverMuteInfo.muteMap[member.id]?.let {
+                val mutedRole = serverMuteInfo.getMutedRole()
+                val duration = it - System.currentTimeMillis()
+
+                if (duration > 1000L) {
+                    try {
+                        member.addRole(mutedRole)
+                    } catch (e: Exception) {
+                        // Ignore it
+                    }
+                } else {
+                    serverMuteInfo.muteMap.remove(member.id)
+                }
+            }
+        }
+    }
+
+    class ServerMuteInfo(val server: Server) {
+        val muteMap = ConcurrentHashMap<Long, Long>() // <Member ID, Unmute Time>
+        val coroutineMap = HashMap<Long, Job>() // <Member ID, Coroutine Job>
+
+        private var mutedRole: Role? = null
+
+        suspend fun getMutedRole() = mutedRole
+            ?: server.roles.findByName("Muted", true)
+            ?: server.createRole {
+                name = "Muted"
+                permissions = PermissionSet(server.roles.everyone.permissions.compile() and 68224001)
+                position = server.members.botUser.roles.map { it.position }.maxOrNull()!!
+            }
+
+        suspend fun startUnmuteCoroutine(
+            member: Member,
+            role: Role,
+            duration: Long
+        ) {
+            coroutineMap[member.id] = muteManagerScope.launch {
+                delay(duration)
+
+                try {
+                    member.removeRole(role)
+                    val bot = Main.client.botUser
+                    member.getPrivateChannel().send {
+                        embed {
+                            field(
+                                "You were unmuted automatically by:",
+                                bot.mention
+                            )
+                            field(
+                                "In the guild:",
+                                server.name
+                            )
+                            color = Colors.SUCCESS.color
+                            footer("ID: ${bot.id}", bot.avatar.url)
+                        }
+                    }
+                } catch (e: Exception) {
+                    // this is fine
+                }
+
+                muteMap.remove(member.id)
+                coroutineMap.remove(member.id)
+            }
+        }
+
+        init {
+            muteManagerScope.launch {
+                delay(5000L)
+                while (isActive) {
+                    for ((id, unmuteTime) in muteMap) {
+                        delay(500L)
+                        if (muteMap.containsKey(id) && !coroutineMap.containsKey(id)) {
+                            val duration = unmuteTime - System.currentTimeMillis()
+                            val member = server.members.find(id) ?: continue
+                            startUnmuteCoroutine(member, getMutedRole(), duration)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    init {
+        muteManagerScope.timer(30000L) {
+            try {
+                delay(30000L)
+                save()
+            } catch (e: Exception) {
+                // this is fine
+            }
+        }
+
+        Main.client.addListener(this)
+    }
+
+}
---
+++ src/main/kotlin/org/kamiblue/botkt/command/commands/moderation/UnmuteCommand.kt
@@ -0,0 +1,105 @@
+package org.kamiblue.botkt.command.commands.moderation
+
+import net.ayataka.kordis.entity.find
+import net.ayataka.kordis.entity.server.member.Member
+import org.kamiblue.botkt.MuteManager
+import org.kamiblue.botkt.PermissionTypes
+import org.kamiblue.botkt.command.BotCommand
+import org.kamiblue.botkt.command.Category
+import org.kamiblue.botkt.command.MessageExecuteEvent
+import org.kamiblue.botkt.utils.Colors
+import org.kamiblue.botkt.utils.MessageSendUtils.error
+
+object UnmuteCommand : BotCommand(
+    name = "unmute",
+    alias = arrayOf("unshut"),
+    category = Category.MODERATION,
+    description = "Now start talking!"
+) {
+    init {
+        user("user") { userArg ->
+            executeIfHas(PermissionTypes.COUNCIL_MEMBER, "Unmute user") {
+                if (server == null) {
+                    message.error("Server is null, are you running this from a DM?")
+                    return@executeIfHas
+                }
+
+                val member = server.members.find(userArg.value) ?: run {
+                    message.error("Member not found!")
+                    return@executeIfHas
+                }
+
+                val serverMuteInfo = MuteManager.serverMap.getOrPut(server.id) { MuteManager.ServerMuteInfo(server) }
+                val mutedRole = serverMuteInfo.getMutedRole()
+
+                when {
+                    serverMuteInfo.muteMap.remove(member.id) != null -> {
+                        sendUnMuteDM(member)
+                        serverMuteInfo.coroutineMap.remove(member.id)?.cancel()
+                        member.removeRole(mutedRole)
+
+                        message.channel.send {
+                            embed {
+                                field(
+                                    "${member.tag} was unmuted by:",
+                                    message.author?.mention.toString()
+                                )
+                                footer("ID: ${member.id}", member.avatar.url)
+                                color = Colors.SUCCESS.color
+                            }
+                        }
+                    }
+
+                    member.roles.contains(mutedRole) -> {
+                        sendUnMuteDM(member)
+                        member.removeRole(mutedRole)
+
+                        message.channel.send {
+                            embed {
+                                description = "Warning: ${member.mention} was not muted using the bot, removed muted role."
+                                field(
+                                    "${member.tag} was unmuted by:",
+                                    message.author?.mention.toString()
+                                )
+                                footer("ID: ${member.id}", member.avatar.url)
+                                color = Colors.WARN.color
+                            }
+                        }
+                    }
+
+                    else -> {
+                        message.error("${member.mention} is not muted")
+                    }
+                }
+            }
+        }
+    }
+
+    private suspend fun MessageExecuteEvent.sendUnMuteDM(member: Member) {
+        try {
+            member.getPrivateChannel().send {
+                embed {
+                    field(
+                        "You were unmuted by:",
+                        message.author?.mention.toString()
+                    )
+                    field(
+                        "In the guild:",
+                        server?.name.toString()
+                    )
+                    color = Colors.SUCCESS.color
+                    footer("ID: ${message.author?.id}", message.author?.avatar?.url)
+                }
+            }
+        } catch (e: Exception) {
+            message.channel.send {
+                embed {
+                    title = "Error"
+                    description = "I couldn't DM that user the unmute, they might have had DMs disabled."
+                    color = Colors.ERROR.color
+                }
+            }
+        }
+    }
+
+}
