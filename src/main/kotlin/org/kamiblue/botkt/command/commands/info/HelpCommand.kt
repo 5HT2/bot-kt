82c07c9ff93b6aa83989fa38521cfc723acccf69
lv <~@l1v.in>
2020-12-14 10:02:26 -0500

    [cleanup] Minor reformat

--- src/main/kotlin/org/kamiblue/botkt/command/commands/info/HelpCommand.kt
+++ src/main/kotlin/org/kamiblue/botkt/command/commands/info/HelpCommand.kt
@@ -49,7 +49,9 @@ object HelpCommand : BotCommand(
                     embed {
                         title = "Help for `${Main.prefix}${command.name}`"
                         field("Description:", command.description)
-                        if (alias.isNotEmpty()) { field("Aliases:",  alias) }
+                        if (alias.isNotEmpty()) {
+                            field("Aliases:", alias)
+                        }
                         field("Syntax:", syntax)
                         color = Colors.PRIMARY.color
                     }
