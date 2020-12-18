661581d56045d07ac75b9e2524f4563b905a3c97
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-12-18 15:38:26 -0500

    [new] Added ;rickroll command (#356)
    * [new] Added ;rickroll command
    
    * extracted the url to a const
    
    * Checks if the url is a cdn url

---
+++ src/main/kotlin/org/kamiblue/botkt/command/commands/fun/RickRollCommand.kt
@@ -0,0 +1,32 @@
+package org.kamiblue.botkt.command.commands.`fun`
+
+import org.kamiblue.botkt.command.BotCommand
+import org.kamiblue.botkt.command.Category
+import org.kamiblue.botkt.utils.MessageUtils.error
+import org.kamiblue.botkt.utils.StringUtils.isUrl
+
+object RickRollCommand : BotCommand(
+    name = "rickroll",
+    alias = arrayOf("rick", "rickastley"),
+    category = Category.FUN,
+    description = "Never gonna give you up, Never gonna let you down..."
+) {
+    private val cdnRegex = "https://cdn.discordapp.com/attachments/(\\d{18})/(\\d{18})/(.*$)".toRegex()
+    private const val urlPrefix = "https://cdn.dircordapp.com/attachments"
+
+    init {
+        string("url") { urlArg ->
+            execute {
+                val url = urlArg.value
+                if (!url.isUrl() || !url.matches(cdnRegex)) {
+                    message.channel.error("Invalid url!")
+                    return@execute
+                }
+                val redirectLink = url.replace(cdnRegex, "$urlPrefix/$1/$2/$3")
+                message.delete()
+                message.channel.send(redirectLink)
+            }
+        }
+    }
+
+}
