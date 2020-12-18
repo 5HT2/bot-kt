f06169b7b192ee084ad862042216f2f20ed9a996
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-12-18 15:22:40 -0500

    Detects image in the message and add the url to embed

--- src/main/kotlin/org/kamiblue/botkt/manager/managers/StartBoardManager.kt
+++ src/main/kotlin/org/kamiblue/botkt/manager/managers/StartBoardManager.kt
@@ -1,6 +1,7 @@
 package org.kamiblue.botkt.manager.managers
 
 import net.ayataka.kordis.entity.channel.TextChannel
+import net.ayataka.kordis.entity.message.Message
 import net.ayataka.kordis.event.events.message.ReactionAddEvent
 import org.kamiblue.botkt.BackgroundScope
 import org.kamiblue.botkt.ConfigType
@@ -9,6 +10,7 @@ import org.kamiblue.botkt.StartBoardConfig
 import org.kamiblue.botkt.entity.Emoji
 import org.kamiblue.botkt.event.events.ShutdownEvent
 import org.kamiblue.botkt.manager.Manager
+import org.kamiblue.botkt.utils.StringUtils.isUrl
 import org.kamiblue.botkt.utils.getReactions
 import org.kamiblue.event.listener.asyncListener
 import org.kamiblue.event.listener.listener
@@ -18,6 +20,8 @@ object StartBoardManager : Manager {
 
     private val startBoardConfig
         get() = ConfigManager.readConfigSafe<StartBoardConfig>(ConfigType.STAR_BOARD, false)
+
+    private val imageExtensionRegex = ".*\\.(jpg|png|gif)".toRegex()
 
     init {
         asyncListener<ReactionAddEvent> {
@@ -30,21 +34,25 @@ object StartBoardManager : Manager {
                     return@asyncListener
                 }
 
-                val reactionUsers = message.getReactions(Emoji("⭐"))
-                val startBoardChannel = cfg.channels[server.id]?: run {
+                val startBoardChannel = cfg.channels[server.id] ?: run {
                     Main.logger.info("Star board channel not found for server ${server.id}")
                     return@asyncListener
                 }
-
+                val reactionUsers = message.getReactions(Emoji("⭐"))
                 Main.logger.debug("Star received, message ${message.id} now has ${reactionUsers.size} star")
 
                 if (reactionUsers.size >= cfg.threshold && !cfg.messages.contains(message.id)) {
                     (server.channels.find(startBoardChannel) as TextChannel).send {
                         embed {
-                            title = ("Message from ${message.author?.name}")
+                            author(
+                                message.author?.name,
+                                "https://discord.com/channels/${server.id}/${channel.id}/${message.id}",
+                                message.author?.avatar?.url
+                            )
                             description = message.content
+                            imageUrl = message.getAttachedImageUrl()
                             color = Color(255, 172, 51)
-                            url = "https://discord.com/channels/${server.id}/${channel.id}/${message.id}"
+                            footer("Message ID: ${message.id}")
                             timestamp = message.timestamp
                         }
                     }
@@ -59,6 +67,16 @@ object StartBoardManager : Manager {
 
         BackgroundScope.add(300000L, "Failed to save starboard config") {
             ConfigManager.writeConfig(ConfigType.STAR_BOARD)
+        }
+    }
+
+    private fun Message.getAttachedImageUrl(): String? {
+        return attachments.firstOrNull()?.let {
+            if (it.isImage) it.url
+            else null
+        } ?: content.run {
+            if (isUrl() && matches(imageExtensionRegex)) this
+            else null
         }
     }
 
