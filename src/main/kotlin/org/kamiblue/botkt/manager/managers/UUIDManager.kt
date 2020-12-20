073f4f86b9e3fd9037ecd9b22ca1a05758bf739f
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-12-20 14:07:33 -0500

    [enhancement] Make UUIDManager load/save on startup/shutdown

--- src/main/kotlin/org/kamiblue/botkt/manager/managers/UUIDManager.kt
+++ src/main/kotlin/org/kamiblue/botkt/manager/managers/UUIDManager.kt
@@ -1,9 +1,28 @@
 package org.kamiblue.botkt.manager.managers
 
+import org.kamiblue.botkt.Main
+import org.kamiblue.botkt.event.events.ShutdownEvent
 import org.kamiblue.botkt.manager.Manager
 import org.kamiblue.capeapi.AbstractUUIDManager
+import org.kamiblue.event.listener.listener
 
-object UUIDManager : AbstractUUIDManager("cache/uuids.json"), Manager {
+object UUIDManager : AbstractUUIDManager("cache/uuid.json"), Manager {
+    init {
+        try {
+            load()
+        } catch (e :Exception) {
+            Main.logger.warn("Failed to load UUID cache", e)
+        }
+
+        listener<ShutdownEvent> {
+            try {
+                load()
+            } catch (e :Exception) {
+                Main.logger.warn("Failed to save UUID cache", e)
+            }
+        }
+    }
+
     override fun logError(message: String) {
         throw UUIDFormatException(message)
     }
