e8f9b1429f394b7ffdc6ad7e3efde390c7265596
lv <~@l1v.in>
2020-12-23 12:44:16 -0500

    [feature] Added `;avatarinfo`

---
+++ src/main/kotlin/org/kamiblue/botkt/command/commands/info/AvatarInfoCommand.kt
@@ -0,0 +1,58 @@
+package org.kamiblue.botkt.command.commands.info
+
+import net.ayataka.kordis.entity.message.Message
+import net.ayataka.kordis.entity.user.User
+import org.kamiblue.botkt.command.BotCommand
+import org.kamiblue.botkt.command.Category
+import org.kamiblue.botkt.utils.Colors
+import org.kamiblue.botkt.utils.MessageUtils.error
+
+object AvatarInfoCommand : BotCommand(
+    name = "avatarinfo",
+    alias = arrayOf("avatar", "av"),
+    description = "Show a users avatar",
+    category = Category.INFO
+) {
+    init {
+        literal("full") {
+            user("user") { userArg ->
+                execute("Send the link to the users avatar") {
+                    message.sendAvatar(userArg.value, true)
+                }
+            }
+
+            execute("Send the link to your avatar") {
+                message.sendAvatar(message.author, true)
+            }
+        }
+
+        execute("Send your avatar in an embed") {
+            message.sendAvatar(message.author)
+        }
+
+        user("user") { userArg ->
+            execute("Show the users avatar in an embed") {
+                message.sendAvatar(userArg.value)
+            }
+        }
+    }
+
+    private suspend fun Message.sendAvatar(user: User?, full: Boolean = false) {
+        user ?: run {
+            channel.error("Mentioned user was null!")
+            return
+        }
+
+        if (full) {
+            channel.send(user.avatar.url)
+        } else {
+            channel.send {
+                embed {
+                    title = user.tag
+                    imageUrl = user.avatar.url + "?size=2048"
+                    color = Colors.PRIMARY.color
+                }
+            }
+        }
+    }
+}
