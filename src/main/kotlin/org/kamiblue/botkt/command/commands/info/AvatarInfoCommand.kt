e87abf6d4f418a33bcbf61ad1d113fa7bc97a653
lv <~@l1v.in>
2020-12-23 13:23:25 -0500

    [cleanup] Cleaned up code in AvatarInfoCommand

--- src/main/kotlin/org/kamiblue/botkt/command/commands/info/AvatarInfoCommand.kt
+++ src/main/kotlin/org/kamiblue/botkt/command/commands/info/AvatarInfoCommand.kt
@@ -1,9 +1,9 @@
 package org.kamiblue.botkt.command.commands.info
 
-import net.ayataka.kordis.entity.message.Message
 import net.ayataka.kordis.entity.user.User
 import org.kamiblue.botkt.command.BotCommand
 import org.kamiblue.botkt.command.Category
+import org.kamiblue.botkt.command.MessageExecuteEvent
 import org.kamiblue.botkt.utils.Colors
 import org.kamiblue.botkt.utils.MessageUtils.error
 
@@ -17,39 +17,39 @@ object AvatarInfoCommand : BotCommand(
         literal("full") {
             user("user") { userArg ->
                 execute("Send the link to the users avatar") {
-                    message.sendAvatar(userArg.value, true)
+                    sendAvatar(userArg.value, true)
                 }
             }
 
             execute("Send the link to your avatar") {
-                message.sendAvatar(message.author, true)
+                sendAvatar(message.author, true)
             }
         }
 
         execute("Send your avatar in an embed") {
-            message.sendAvatar(message.author)
+            sendAvatar(message.author)
         }
 
         user("user") { userArg ->
             execute("Send the users avatar in an embed") {
-                message.sendAvatar(userArg.value)
+                sendAvatar(userArg.value)
             }
         }
     }
 
-    private suspend fun Message.sendAvatar(user: User?, full: Boolean = false) {
+    private suspend fun MessageExecuteEvent.sendAvatar(user: User?, full: Boolean = false) {
         user ?: run {
             channel.error("Mentioned user was null!")
             return
         }
 
         if (full) {
-            channel.send(user.avatar.url + "?size=2048")
+            channel.send("${user.avatar.url}?size=2048")
         } else {
             channel.send {
                 embed {
                     title = user.tag
-                    imageUrl = user.avatar.url + "?size=2048"
+                    imageUrl = "${user.avatar.url}?size=2048"
                     color = Colors.PRIMARY.color
                 }
             }
