135f23a94a2ca20e2c01af4a362779e120c2dd46
Dominika <sokolov.dominika@gmail.com>
2020-12-24 11:53:22 -0500

    [bump] v1.5.8

--- src/main/kotlin/org/kamiblue/botkt/Main.kt
+++ src/main/kotlin/org/kamiblue/botkt/Main.kt
@@ -24,7 +24,7 @@ import kotlin.system.exitProcess
 
 object Main {
 
-    const val currentVersion = "v1.5.7"
+    const val currentVersion = "v1.5.8"
 
     @Suppress("EXPERIMENTAL_API_USAGE")
     val mainScope = CoroutineScope(newSingleThreadContext("Bot-kt Main"))
