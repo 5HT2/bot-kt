b11ec4c1e6af4af21d32243f69f528d85351e057
Xiaro <62033805+Xiaro@users.noreply.github.com>
2021-01-22 15:50:06 -0500

    config field in ResponseManager should be private

--- src/main/kotlin/org/kamiblue/botkt/manager/managers/ResponseManager.kt
+++ src/main/kotlin/org/kamiblue/botkt/manager/managers/ResponseManager.kt
@@ -10,7 +10,7 @@ import org.kamiblue.event.listener.asyncListener
 
 object ResponseManager : Manager {
 
-    val config get() = ConfigManager.readConfigSafe<ResponseConfig>(ConfigType.RESPONSE, false)
+    private val config get() = ConfigManager.readConfigSafe<ResponseConfig>(ConfigType.RESPONSE, false)
 
     init {
         asyncListener<MessageReceiveEvent> { event ->
