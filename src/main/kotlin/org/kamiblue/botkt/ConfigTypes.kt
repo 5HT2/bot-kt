5feb78641a9fd2dec12aa8f127285dcc860ff816
Dominika <sokolov.dominika@gmail.com>
2021-01-22 22:48:11 -0500

    [fix] Potential bug

--- src/main/kotlin/org/kamiblue/botkt/ConfigTypes.kt
+++ src/main/kotlin/org/kamiblue/botkt/ConfigTypes.kt
@@ -155,6 +155,6 @@ class ArchivedChannelsConfig(
  */
 class ResponseConfig(
     val responses: List<ResponseManager.Response> = emptyList(),
-    val roleIgnorePrefix: String = ":",
+    val roleIgnorePrefix: String? = ":",
     val ignoreChannels: HashSet<Long>?,
 )
