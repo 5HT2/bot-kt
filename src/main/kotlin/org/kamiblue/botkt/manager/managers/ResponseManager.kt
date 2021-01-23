0b58e20a33599d3499395061a2f68cfb0b85d051
Dominika <sokolov.dominika@gmail.com>
2021-01-22 19:09:15 -0500

    fix

--- src/main/kotlin/org/kamiblue/botkt/manager/managers/ResponseManager.kt
+++ src/main/kotlin/org/kamiblue/botkt/manager/managers/ResponseManager.kt
@@ -1,5 +1,6 @@
 package org.kamiblue.botkt.manager.managers
 
+import com.google.gson.GsonBuilder
 import net.ayataka.kordis.event.events.message.MessageReceiveEvent
 import org.kamiblue.botkt.ConfigType
 import org.kamiblue.botkt.Main
@@ -8,6 +9,33 @@ import org.kamiblue.botkt.manager.Manager
 import org.kamiblue.botkt.utils.Colors
 import org.kamiblue.botkt.utils.tryDelete
 import org.kamiblue.event.listener.asyncListener
+
+fun main() {
+    val gson = GsonBuilder().setPrettyPrinting().create()
+
+    val elytraResponse = ResponseManager.Response(
+        regexes = listOf(".*(elytra|elytra.{0,2}light|elytra.{0,2}+|elytra.{0,2}fly).*", "(settings|config|configure)"),
+        deleteMessage = false,
+        responseDescription = "On 2b2t, Use the settings posted in <#634012886930423818>.\\n\\nOn non-2b2t servers, use Control mode with *default settings*.\\nIf it's still not working, make sure you're not using NoFall, AntiHunger or any other movement related mods from **other** clients, such as Sprint in Rage mode.",
+        responseTitle = "ElytraFlight help",
+        responseColor = Colors.PRIMARY,
+        whitelistReplace = null,
+        ignoreRoles = null
+    )
+
+    val discordResponse = ResponseManager.Response(
+        regexes = listOf("(d.{0,3}.{0,3}s.{0,3}c.{0,3}.{0,3}r.{0,3}d).{0,7}(gg|com.{0,3}invite)"),
+        deleteMessage = true,
+        responseDescription = "You don't have permission to send Discord Invites",
+        responseTitle = "Rule 5",
+        responseColor = Colors.ERROR,
+        ignoreRoles = hashSetOf(754406610239225927),
+        whitelistReplace = null
+    )
+
+    val config = ResponseConfig(listOf(elytraResponse, discordResponse))
+    println(gson.toJson(config))
+}
 
 object ResponseManager : Manager {
 
@@ -49,6 +77,7 @@ object ResponseManager : Manager {
 
                     if (response.deleteMessage) {
                         event.message.tryDelete()
+                        break // Once the message has been deleted we want to stop responding to the regexes
                     }
                 }
             }
