34b638c3ed3a71d563a45d3c89a716aa40a38a02
Dominika <sokolov.dominika@gmail.com>
2021-01-22 21:46:59 -0500

    [bump] v1.8.2

--- src/main/kotlin/org/kamiblue/botkt/Main.kt
+++ src/main/kotlin/org/kamiblue/botkt/Main.kt
@@ -28,7 +28,7 @@ import kotlin.system.exitProcess
 
 object Main {
 
-    const val currentVersion = "v1.8.1"
+    const val currentVersion = "v1.8.2"
 
     val startUpTime: Instant = Instant.now()
     @Suppress("EXPERIMENTAL_API_USAGE")
